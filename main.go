63e9ebf917c36d1deb681921f370b3e75eb233d7
tesiaka <tsiaka7197@stu.kemu.ac.ke>
2024-07-23 16:52:42 +0300

    update:update dir for image server

--- main.go
+++ main.go
@@ -12,10 +12,15 @@ func main() {
 	fs := http.FileServer(http.Dir("static"))
 	mux.Handle("/static/", http.StripPrefix("/static/", fs))
 
+	img := http.FileServer(http.Dir("images"))
+	mux.Handle("/images/", http.StripPrefix("/images/", img))
+
 	mux.HandleFunc("/", asciifunc.Router)
 	mux.HandleFunc("/asciihandler", asciifunc.Trial)
 	mux.HandleFunc("/400", asciifunc.BadRequest)
 	mux.HandleFunc("/400/badrequest", asciifunc.LoadContentHandler)
+	mux.HandleFunc("/500", asciifunc.StatusInternalServerError)
+	mux.HandleFunc("/internalservererror", asciifunc.LoadContentHandlerInternalError)
 
 	log.Println("starting server on: http://localhost:8080")
 	log.Fatal(http.ListenAndServe("localhost:8085", mux))
