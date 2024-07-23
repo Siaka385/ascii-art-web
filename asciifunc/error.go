76670f899039d7a32f702d1b638870ec80b6fad7
tesiaka <tsiaka7197@stu.kemu.ac.ke>
2024-07-23 16:56:57 +0300

    update:uodate on 500 page error

--- asciifunc/error.go
+++ asciifunc/error.go
@@ -16,8 +16,19 @@ func RenderTemplate(w http.ResponseWriter, tmpl string, data interface{}, status
 	t.Execute(w, data)
 }
 
-func StatusInternalServerError(w http.ResponseWriter) {
-	RenderTemplate(w, "500.html", nil, http.StatusInternalServerError)
+func StatusInternalServerError(w http.ResponseWriter, r *http.Request) {
+	tmp, err := template.ParseFiles("500.html")
+	if err != nil {
+		http.Error(w, "Internal server Error", http.StatusInternalServerError)
+		return
+	}
+	if r.URL.Query().Get("error") == "true" {
+		err = tmp.Execute(w, nil)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+	}
 }
 
 func BadRequest(w http.ResponseWriter, r *http.Request) {
@@ -34,6 +45,11 @@ func BadRequest(w http.ResponseWriter, r *http.Request) {
 func LoadContentHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusBadRequest)
+}
+
+func LoadContentHandlerInternalError(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusInternalServerError)
 }
 
 func Pagenofound(w http.ResponseWriter) {
