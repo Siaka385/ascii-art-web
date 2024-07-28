4f3f5e28d2b55add5c7f71acccc4cf469ccfaf1b
tesiaka <tsiaka7197@stu.kemu.ac.ke>
2024-07-28 17:27:31 +0300

    feat:reduce main size

--- main.go
+++ main.go
@@ -12,18 +12,8 @@ func main() {
 	fs := http.FileServer(http.Dir("static"))
 	mux.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	// img := http.FileServer(http.Dir("images"))
-	// mux.Handle("/images/", http.StripPrefix("/images/", img))
-
 	mux.HandleFunc("/", asciifunc.Router)
-	mux.HandleFunc("/asciihandler", asciifunc.Trial)
-	mux.HandleFunc("/400", asciifunc.BadRequest)
-	mux.HandleFunc("/400/badrequest", asciifunc.LoadContentHandler)
-	mux.HandleFunc("/500", asciifunc.StatusInternalServerError)
-	mux.HandleFunc("/internalservererror", asciifunc.LoadContentHandlerInternalError)
-	mux.HandleFunc("/405", asciifunc.Wrongmethodused)
-	mux.HandleFunc("/404banner", asciifunc.StatusUnavailableBanner)
-	mux.HandleFunc("/unavailablebanner", asciifunc.Setstatus)
+	
 
 	log.Println("starting server on: http://localhost:8087")
 	log.Fatal(http.ListenAndServe("localhost:8086", mux))
