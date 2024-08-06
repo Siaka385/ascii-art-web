a10159d177453cf492690d819f1d99f0c67c28e4
tesiaka <tsiaka7197@stu.kemu.ac.ke>
2024-08-06 11:12:14 +0300

    style&fix:style the links and fix missing template error

--- main.go
+++ main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"ascii-art/asciifunc"
 )
 
 func main() {
+	myargs := os.Args[1:]
+	if len(myargs) > 0 {
+		fmt.Println("Too many args used in the terminal")
+		os.Exit(1)
+	}
+
 	mux := http.NewServeMux()
 	fs := http.FileServer(http.Dir("static"))
 	mux.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	mux.HandleFunc("/", asciifunc.Router)
 
-	log.Println("starting server on: http://localhost:8087")
-	log.Fatal(http.ListenAndServe("localhost:8086", mux))
+	log.Println("starting server on: http://localhost:8086")
+	log.Fatal(http.ListenAndServe("localhost:8087", mux))
 }
